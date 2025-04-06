ed2523abc1aad0b9c7e044df8a60accea1e7faf0
Lester Bulay <163224532+Loyzkie2005@users.noreply.github.com>
2025-04-06 11:50:26 +0800

    UPDATING SOME FILES

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.querySelectorAll('.navbar a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
-        });
+
+document.addEventListener('DOMContentLoaded', function() {
+  //  Lester's CV
+  const jhonPeterButton = document.getElementById('jhon-peter-button');
+  // Jhon Peter's CV
+  const lesterButton = document.getElementById('lester-button');
+  
+  if (jhonPeterButton) {
+    jhonPeterButton.addEventListener('click', function() {
+      document.querySelector('.container').classList.add('flip-animation');
+      setTimeout(function() {
+        window.location.href = 'Codilla.html';
+      }, 1000);
     });
+  }
+  
+  if (lesterButton) {
+    lesterButton.addEventListener('click', function() {
+      document.querySelector('.container').classList.add('flip-animation');
+      setTimeout(function() {
+        window.location.href = 'Lester.html';
+      }, 1000);
+    });
+  }
 });
